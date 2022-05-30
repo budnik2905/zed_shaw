7c418a20ce1fc8bb1b426c5257ad7475ba893f41
budnik2905 <budnik2905@gmail.com>
2022-05-30 14:48:25 +0300

    Входные данные

---
+++ zed_shaw/zed_shaw1.4.py
@@ -0,0 +1,14 @@
+print("Сколько тебе лет?", end='')
+age = input()
+print("Каков твой рост ?", end='')
+height = input()
+print("Сколько ты весишь ?", end='')
+weight = input()
+
+print(f"Итак, тебе {age} лет, в тебе {height} см роста и {weight} кг веса")
+
+age = input("Сколько тебе лет ? ")
+height = input("Каков твой рост ?")
+weight = input("Сколько ты весишь ?")
+
+print(f"Итак, тебе {age} лет, в тебе {height} см роста и {weight} кг веса.")
